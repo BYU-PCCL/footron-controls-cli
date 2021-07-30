572aece9c1aeb633d56257e4c88365065b9ac1ef
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-07-29 18:08:57 -0600

    Add fonts available in controls

--- src/renderer.ts
+++ src/renderer.ts
@@ -83,12 +83,17 @@ export async function startRenderer({
       <html>
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
           <style>
           .ftcontrols-error {
             color: #e00;
           }
           </style>
+          <!-- Normalize -->
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+          <!-- Default fonts -->
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:wght@600;700&family=Source+Code+Pro:wght@600;700&display=swap" rel="stylesheet">
         </head>
         <body>
           <div id="root"></div>
